ad3be84e254af850ba8624cfa599304cf17e377a
Yılmaz Ünal <52472778+ylmzunal@users.noreply.github.com>
2024-12-17 04:04:00 +0300

    bugs fixed

--- swe573_fall_2024/swe573_fall_2024/settings.py
+++ swe573_fall_2024/swe573_fall_2024/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-from google.oauth2 import service_account
+# from google.oauth2 import service_account
 from dotenv import load_dotenv
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
